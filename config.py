9e9898a92b63b393cb8338ce80577da140972da6
Prin718 <sakarsandhya1987@gmail.com>
2025-01-20 15:03:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,14 +9,14 @@ if os.path.exists("local.env"):
 API_ID = int(getenv("API_ID", "14050586")) #optional
 API_HASH = getenv("API_HASH", "42a60d9c657b106370c79bb0a8ac560c") #optional
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5738579437").split()))
-OWNER_ID = int(getenv("OWNER_ID", "5738579437"))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "8093150680").split()))
+OWNER_ID = int(getenv("OWNER_ID", "7159158466"))
 MONGO_URL = getenv("MONGO_URL", "mongodb+srv://Krishna:pss968048@cluster0.4rfuzro.mongodb.net/?retryWrites=true&w=majority")
 BOT_TOKEN = getenv("BOT_TOKEN", "")
 ALIVE_PIC = getenv("ALIVE_PIC", 'https://envs.sh/oKU.jpg')
 ALIVE_TEXT = getenv("ALIVE_TEXT")
 PM_LOGGER = getenv("PM_LOGGER")
-LOG_GROUP = getenv("LOG_GROUP","-1001992970818")
+LOG_GROUP = getenv("LOG_GROUP","-1002482767248")
 GIT_TOKEN = getenv("GIT_TOKEN") #personal access token
 REPO_URL = getenv("REPO_URL", "https://github.com/Rishuz/Slove")
 BRANCH = getenv("BRANCH", "main") #don't change
